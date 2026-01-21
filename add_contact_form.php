b494fb72c82429432353db90faa301b54020f5b6
Douglas Jasper <doug.jasper@trios.com>
2026-01-21 10:14:10 -0500

    Added Add Contact Form

---
+++ add_contact_form.php
@@ -0,0 +1,61 @@
+<?php
+    session_start();
+
+    require("database.php");    
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Contact Manager - Add Contact</title>
+        <link rel="stylesheet" type="text/css" href="css/contact.css" />
+    </head>
+
+    <body>
+        <?php include("header.php"); ?>
+
+        <main>
+            <h2>Add Contact</h2>
+
+            <form action="add_contact.php" method="post" id="add_contact_form" enctype="multipart/form-data">
+
+                <div id="data">
+
+                    <label>First Name:</label>
+                    <input type="text" name="first_name" /><br />
+
+                    <label>Last Name:</label>
+                    <input type="text" name="last_name" /><br />
+
+                    <label>Email Address:</label>
+                    <input type="text" name="email_address" /><br />
+
+                    <label>Phone Number:</label>
+                    <input type="text" name="phone_number" /><br />
+
+                    <label>Status:</label><br />
+                    <input type="radio" name="status" value="member" />Member<br />
+                    <input type="radio" name="status" value="nonmember" />Non-Member<br /><br />
+
+                    <label>Birth date:</label>
+                    <input type="date" name="dob" /><br />
+
+                </div>
+
+                <div id="buttons">
+                   <label>&nbsp;</label>
+                   <input type="submit" value="Save Contact" /><br /> 
+                </div>
+
+            </form>            
+
+            <p><a href="index.php">View Contact List</a></p>
+
+        </main>
+
+        <?php include("footer.php"); ?> 
+
+    </body>
+</html>       
